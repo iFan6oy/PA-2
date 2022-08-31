2e95ec5f3cfe556a378229db913f46d8670c6070
Kyteinflight <111820920+Kyteinflight@users.noreply.github.com>
2022-08-31 00:47:37 -0500

    Update PA2.py
    Some issues with the base file not working as intended were fixed

--- PA2.py
+++ PA2.py
@@ -1,12 +1,13 @@
+import csv
 import random
 import time
 
 # Python program for implementation of MergeSort
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
+# Merges two subarrays of array_i[].
+# First subarray is array_i[l..m]
+# Second subarray is array_i[m+1..r]
 
-def merge(array, l, m, r):
+def merge(array_i, l, m, r):
     n1 = m - l + 1
     n2 = r - m
 
@@ -16,86 +17,86 @@ def merge(array, l, m, r):
 
     #Data is copied into temporary arrays L[] and R[]
     for i in range(0, n1):
-        L[i] = array[l + i]
+        L[i] = array_i[l + i]
 
     for j in range (0, n2):
-        R[j] = array[m + 1 + j]
+        R[j] = array_i[m + 1 + j]
 
-    # Temp arrays are merged back into arr[l...r]
+    # Temp arrays are merged back into array_i[l...r]
     i = 0 # First index of the first subarray
     j = 0 # First index of the second subarray
     k = l # First index of the merged subarray
 
     while i < n1 and j < n2 :
         if L[i] <= R[j]:
-            array[k] = L[i]
+            array_i[k] = L[i]
             i += 1
         else:
-            array[k] = R[j]
+            array_i[k] = R[j]
             j += 1
 
         k += 1
 
      # Copies the leftover elements of L[] if any are remaining
     while i < n1:
-         array[k] = L[i]
+         array_i[k] = L[i]
          i += 1
          k += 1
 
      # Copies the leftover elements of R[] if any are remaining
     while j < n2:
-         array[k] = R[j]
+         array_i[k] = R[j]
          j +=1
          k +=1
 
 # l is for the left index and r is for the right index of the subarray of the array to be sorted
-def mergeSort(array, l, r):
+def mergeSort(array_i, l, r):
     if l < r:
 
         # Same as (l + r)/2 but it avoids overflow for large values for l and h
         m = int((l + (r - 1))/2)
 
         # Sort first and second halves
-        mergeSort(array, l, m)
-        mergeSort(array, m + 1, r)
-        merge(array, l, m, r)
+        mergeSort(array_i, l, m)
+        mergeSort(array_i, m + 1, r)
+        merge(array_i, l, m, r)
 
 def main():
-    size = 1000
+    size = 0
     lists = [0] * 10
-
-    for i in range(9):
-        array = []
-        for j in range(size):
-            array.append(random.randint(0, 100))
-
-        size += 1000
-        lists[i] = array
 
     ch = 'y'
     while ch == 'y' or ch == 'Y':
-        n = int(input("Enter a number from 1 to 9 : "))
-        if n < 0 or n > 9:
-            print("Invalid input, please try again!")
-        else:
-            array = lists[i]
+        
+            n = int(input("Enter a number from 1 to 9 : "))
+            if n < 0 or n > 10:
+                print("Invalid input, please try again!")
+            else:
+                for i in range(9):
+                    array_i = []
+                    for j in range(size):
+                        array_i.append(random.randint(0, 100))
 
-            n = len(array)
-            print("The array is")
-            for i in range(n):
-                print (array[i], end = " ")
+                    size = 1000 * n
+                    lists[i] = array_i
+                    array_i = lists[i]
 
-            start_time = time.time()
-            mergeSort(array, 0, n - 1)
-            end_time = time.time()
+                n = len(array_i) 
+                print("The array is")
+                for i in range(n):
+                    print (array_i[i], end = " ")
 
-            print("\n\nSorted array is")
-            for i in range(n):
-                print(array[i], end = " ")
+                start_time = time.time()
+                mergeSort(array_i, 0, n - 1)
+                end_time = time.time()
 
-            print("\n\nTime taken = " + str(end_time - start_time) + " seconds")
+                print("\n\nSorted array is")
+                for i in range(n):
+                    print(array_i[i], end = " ")
 
-            ch = input("\n\nDo you wish to continue? (y/n) : ")[0]
+                print("\n\nTime taken = " + str(end_time - start_time) + " seconds")
+
+                ch = input("\n\nDo you wish to continue? (y/n) : ")[0]
 
 if __name__ == "__main__":
    main()
